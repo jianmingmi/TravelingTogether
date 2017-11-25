7f73363863bc094aa6bf1f96271add5548d8fc09
jmm <15951723371@189.cn>
2017-11-25 13:17:05 +0800

    1.删除test模块 2.修改中心数据库sql

--- src/product/index/IndexController.java
+++ src/product/index/IndexController.java
@@ -1,15 +1,12 @@
 package product.index;
 
 import base.BaseController;
-import product.test.model.Test;
-
-import java.util.List;
 
 /**
  * Created by Administrator on 2017/11/12.
  */
 public class IndexController extends BaseController {
-    public void index(){
+    public void index() {
         renderJsp("/main.jsp");
     }
 }
